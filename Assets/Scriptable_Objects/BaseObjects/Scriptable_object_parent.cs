97c27f76368350c4b79d3e5546b5ef02e94b31ac
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-11-26 23:00:34 -0500

    updated scriptable object parent with setter and gettter. use these to access the fields

--- Assets/Scriptable_Objects/BaseObjects/Scriptable_object_parent.cs
+++ Assets/Scriptable_Objects/BaseObjects/Scriptable_object_parent.cs
@@ -10,4 +10,11 @@ public class Scriptable_object_parent : ScriptableObject
     public int accuracy;
     public int reaction;
     public int money;
+
+    public int _Health{get{return(health);}set{health=value;}}
+    public int _Confidence { get { return (confidence); } set { confidence=value; } }
+    public int _Accuracy { get { return (accuracy); } set { accuracy=value; } }
+    public int _Reaction { get { return (reaction); } set { reaction=value; } }
+    public int _Money{get{return(money);} set{money = value;} }
+
 }
