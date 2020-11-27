9aecf632cd00ca701c663c4802b1c7b4e9a4395e
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-11-26 20:00:49 -0500

    made scriptble object parent class
    4 base scriptable objects will derive from this so i don't need to remake things

---
+++ Assets/Scriptable_Objects/BaseObjects/Scriptable_object_parent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Scriptable_object_parent : ScriptableObject
+{
+    public int health;
+    public int confidence;
+    public int accuracy;
+    public int reaction;
+    public int money;
+}
