7ab5cb7aa55faa915bbb1f8ab0907bab4fba3467
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-11-26 20:02:29 -0500

    created sabrina inherit

---
+++ Assets/Scriptable_Objects/BaseObjects/Sabrina.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Sabrina", menuName = "Create Instance", order =1)]
+public class Sabrina : Scriptable_object_parent
+{
+ 
+}
