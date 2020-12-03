ff9ce79b09c180042d5ba48c00d346a99f7a68aa
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-12-02 19:06:30 -0500

    added abstract state class for implmenetation latert
    defaults to yield break as not all things may end up using this. also we can extend it later if need be

---
+++ Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State  
+{
+    public virtual IEnumerator Start()
+    {
+        yield break;
+    }
+    public virtual IEnumerator ReadyForUpdate()
+    {
+        yield break;
+    }
+    public virtual IEnumerator WaitingForInput()
+    {
+        yield break;
+    }
+}
