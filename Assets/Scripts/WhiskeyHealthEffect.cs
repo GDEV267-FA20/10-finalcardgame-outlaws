8b6a510caa61d2b2cd52bc6cc8733e4a821b290a
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-12-14 23:06:47 -0500

    Work that I can't work on more.

---
+++ Assets/Scripts/WhiskeyHealthEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhiskeyHealthEffect : MonoBehaviour
+{
+    public Scriptable_object_parent healthSpriteGen;
+    public Image HealthWhiskey;
+
+    public void healthChange()
+    {
+        for(int i = 0; i > healthSpriteGen.temphealth; i++)
+        {
+            HealthWhiskey.enabled = false;
+        }
+    }
+}
